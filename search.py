e0103835f2b5ab6e5e87467bf3383226c4e3b598
Daisuke KONISHI <skydai1151@gmail.com>
2019-01-31 00:44:24 +0900

    Add historical WIP

--- search.py
+++ search.py
@@ -17,9 +17,28 @@ class PytrendsSearch(object):
         df.plot(figsize=(15, 3), lw=.7)
         plt.show()
 
+    # def historical_interest(self, keywords):
+    #     df = self.pytrends.get_historical_interest(
+    #                                 keywords,
+    #                                 year_start=2019,
+    #                                 month_start=1,
+    #                                 day_start=1,
+    #                                 hour_start=0,
+    #                                 year_end=2019,
+    #                                 month_end=12,
+    #                                 day_end=31,
+    #                                 hour_end=0,
+    #                                 cat=0,
+    #                                 geo=self.geo,
+    #                                 gprop='',
+    #                                 sleep=0)
+    #     df.plot(figsize=(15, 3), lw=.7)
+    #     plt.show()
+
 
 if __name__ == '__main__':
     keyword_list = ["Python"]
 
     print('============= Start Search ===================')
     df = PytrendsSearch().search_result(keyword_list)
+    # df = PytrendsSearch().historical_interest(keyword_list)
